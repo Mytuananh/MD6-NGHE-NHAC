b3cd52a15eed3f72490d25c838b163f8c2556923
Your Name <you@example.com>
2022-01-15 10:25:29 +0700

    list song

--- src/app/song-manager/list-song/list-song.component.ts
+++ src/app/song-manager/list-song/list-song.component.ts
@@ -1,8 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
+import { Component, OnInit } from '@angular/core';
+import {PageEvent} from '@angular/material/paginator';
 import {Song} from '../../model/Song';
 import {SongService} from '../../service/song.service';
-import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-list-song',
@@ -10,21 +9,37 @@ import {MatTableDataSource} from '@angular/material/table';
   styleUrls: ['./list-song.component.scss']
 })
 export class ListSongComponent implements OnInit {
-  displayedColumns: string[] = ['ID', 'Name', 'Description', 'File', 'Singer', 'musician', 'count', 'countLike', 'Edit', 'Delete'];
-  // dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  dataSource: any;
-  songs: Song[];
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  totalElements: any = 0;
+  songs: Song[] = [];
+  loading: boolean;
   constructor(private songService: SongService) { }
 
   ngOnInit(): void {
-    this.getListSong();
+    this.getListRequest({page: 0, size: 3});
   }
-  getListSong() {
-    this.songService.listSong().subscribe(listSong => {
-      this.songs = listSong;
-      this.dataSource = new MatTableDataSource<Song>(this.songs);
-      this.dataSource.paginator = this.paginator;
+  private getListRequest(request) {
+    this.loading = true;
+    this.songService.pageSong(request).subscribe(data => {
+      console.log('data --> ', data);
+      this.songs = data.content;
+      console.log('data[content] ---->', data.content);
+      this.totalElements = data.totalElements;
+      console.log('data[totalElements] == ', data.totalElements);
+      this.loading = false;
+    }, error => {
+      this.loading = false;
     });
+  }
+
+  nextPage(event: PageEvent) {
+    console.log('event -->', event);
+    const request = {};
+    // @ts-ignore
+    request.page = event.pageIndex.toString();
+    // @ts-ignore
+    request.size = event.pageSize.toString();
+    // @ts-ignore
+    console.log('request[size]', request.size);
+    this.getListRequest(request);
   }
 }
